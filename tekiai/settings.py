4a9b11748b9ae8a408bdfe1435a28f4439879984
Alejandro CR <contacto@alejandrocr.co>
2025-06-08 16:34:24 -0500

    add STATIC_ROOT and MEDIA_ROOT settings for static and media files

--- tekiai/settings.py
+++ tekiai/settings.py
@@ -121,9 +121,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Media files (User-uploaded content)
 
 MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
